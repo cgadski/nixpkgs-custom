a42d0fbe6614a31342695bec56823a4573eb61c3
Magnetic Duck <zenmags@gmail.com>
2015-06-07 15:44:26 +0000

    made presentable?

--- makeConfortable.nix
+++ makeConfortable.nix
@@ -23,7 +23,9 @@ let
         mkdir -p $out
         mkdir -p $out/$resources
         ${unlines (map (lib: "ln -s ${lib}/lib/*.so* $out/$resources/") libs)}
-        ln -s /nix/store/2q4nir7g03b7qidk9m2r9wcq3ga1fv65-gcc-4.8.4/lib64/libstdc++.so.6 $out/$resources/libstdc++.so.6
+
+        ln -s /nix/store/2q4nir7g03b7qidk9m2r9wcq3ga1fv65-gcc-4.8.4/lib64/libstdc++.so.6 $out/$resources/libstdc++.so.6 #FIXME: figure this shit out
+
         cp -r $src/* $out/
         
         # patching file
