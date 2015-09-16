001a4f7bedb40292a2d54b864e55ca5ad92db4ca
Chris Gadzinski <zenmags@gmail.com>
2015-09-16 12:45:18 +0000

    fixed renoise

--- renoise/default.nix
+++ renoise/default.nix
@@ -14,14 +14,14 @@ let
       unpackPhase = "tar -xf $src";
 
       src =
-        aseert builtins.currentSystem == "x86_64-linux";
+        assert builtins.currentSystem == "x86_64-linux";
         if demo then rns-installer
         else
           requireFile {
             url = "http://backstage.renoise.com/frontend/app/index.html#/login";
             name = "rns_3_0_1_linux_x86_64.tar.gz";
             sha256 = "0n23m0f3pkwgicgvvg4hr7wa1jdk0py91fndda5986ri0km4j6af";
-          }
+          };
 
       installPhase = ''
         ${if demo then "cd Renoise_3_0_1_Demo_x86_64" else ""}
